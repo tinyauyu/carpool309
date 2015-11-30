c86f6afe2ecc7cdd063ab999f5a7a8cd6442e939
yang ran <yangran1210@gmail.com>
2015-11-30 15:49:58 -0500

    update chat window so no conflict

--- public/js/chatWindow.js
+++ public/js/chatWindow.js
@@ -1,64 +1,48 @@
-$(document).ready(function () {
-    $(".slide").click(function() {
-        if($(this).children().hasClass("glyphicon-chevron-down")) {
-            $(this).children().removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        } else {
-            $(this).children().removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-        }
-    });
-});
 
-$(document).ready(function() {
-    $('#removeBox').click(function () {
-        $("#resultPanel").addClass("hidden");
-        location.reload();
-    });
+$(document).on('click','.slide' ,function() {
+    if($(this).children().hasClass("glyphicon-chevron-down")) {
+        $(this).children().removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    } else {
+        $(this).children().removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    }
 });
 
 
-// $(document).ready( function(){
-//     $.ajax({
-//         type: "GET",
-//         url: "/api/users/"+$('#profilePic_preview').data('id')+"/profilePic",
-//         success: function(data){
-//           $('#receiverImg').attr("src",data);
-//         },
-//         error: function(jqxhr, textStatus, errorThrown){
-//           alert(errorThrown);
-//         }
-//     });
-// });
+$(document).on('click', '#removeBox', function () {
+    $("#resultPanel").addClass("hidden");
+    location.reload();
+});
 
-// var socket = io();
 var sender = $('.navbar-brand').attr('loggedInUser');
 socket.emit('register', {sender: sender});
 var receiver;
 
-$(document).ready( function(){
-    $('form#inputMsg').submit(function() {
-        var msg = $('#msg').val();
-        var attr = $('button#chat').attr('receiver');
-        if(typeof attr !== typeof undefined && attr !== false){
-            receiver =  $('button#chat').attr("receiver");
-        } else {
-            receiver = $('#resultPanel').attr('sendto');
-        };
-        var ele = $('<p>');
-        $('#messages').append(ele.text(msg));
-        ele.attr('id', 'msgYouSend');
-        var data = {
-            sender: sender,
-            receiver: receiver,
-            msg: msg
-        };
 
-        socket.emit('chat message', data);
-        $('#msg').val('');
-        return false;
-    });
+$(document).on('click', '#msgButton', function() {
+    console.log("aaaaaaa");
+    var msg = $('#msg').val();
+    var attr = $('button#chat').attr('receiver');
+    if(typeof attr !== typeof undefined && attr !== false){
+        receiver =  $('button#chat').attr("receiver");
+    } else {
+        receiver = $('#resultPanel').attr('sendto');
+    };
+    var ele = $('<p>');
+    $('#messages').append(ele.text(msg));
+    ele.attr('id', 'msgYouSend');
+    var data = {
+        sender: sender,
+        receiver: receiver,
+        msg: msg
+    };
+
+    socket.emit('chat message', data);
+    $('#msg').val('');
+    return false;
 });
 
 socket.on('chat message', function(data) {
+    console.log("bbbbbbbbbb");
     if ($("#resultPanel").is(':visible') && $("#resultPanel").attr('sendto') == data.sender) {
         var ele = $('<p>');
         $('#messages').append(ele.text(data.msg));
@@ -70,3 +54,5 @@ socket.on('chat message', function(data) {
         });
     }
 });
+
+
