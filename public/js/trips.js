40b3891765c4add88a59012627f0fcdaadfc1d45
freeperson6 <stevexu.zhang@mail.utoronto.ca>
2015-12-06 16:14:43 -0500

    add comments for more js files
    add comments for more js files

--- public/js/trips.js
+++ public/js/trips.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+/*----------------------------------------------------------------
+If certain trip clicked, get the trip id and go to 
+speicially designed user profile page
+-----------------------------------------------------------------*/
   $('.trip-row').click(function(){
       var ids = $(this).data('id');
       var url = "/users/" + ids;
       window.location.href = url;
   });
 
+/*----------------------------------------------------------------
+Same thing as above, but click on similar users
+-----------------------------------------------------------------*/
    $('.similar-trip-row').click(function(){
       var ids = $(this).data('id');
       var url = "/users/" + ids;
